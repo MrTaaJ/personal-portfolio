79ca4c5e53c0f7456f9cb848acf63ac7c7bae54d
Abdu Wasi' Adelakun <wasleadwears4u2c@gmail.com>
2021-11-05 18:28:57 +0100

    chore: Add node modules foir debugging

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const navUl = document.getElementById('mobile-nav-tog');
 const cancelBut = document.getElementById('cancel-butt');
 
 hamburger.addEventListener('click', () => {
-    navUl.classList.add('active');
+  navUl.classList.add('active');
 });
 
 cancelBut.addEventListener('click', () => {
-    navUl.classList.remove('active');
+  navUl.classList.remove('active');
 });
