e9363531f6f105e2ad4c9732093354c61c849293
Saheed Tajudeen <saheed.taaj@gmail.com>
2021-11-09 11:10:07 +0100

    feat: Completed the first modal pop-up (both html and css with a test javascript)

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const cancelBut = document.getElementById('cancel-butt');
 const cancelBut1 = document.getElementById('cancel1-butt');
 const cancelBut2 = document.getElementById('cancel2-butt');
 const cancelBut3 = document.getElementById('cancel3-butt');
+const popBut = document.querySelectorAll('#pop-button-1');
+const popUl = document.querySelectorAll('#pop-proj-1-back');
 
 hamburger.addEventListener('click', () => {
   navUl.classList.add('active');
@@ -19,4 +21,10 @@ cancelBut2.addEventListener('click', () => {
 });
 cancelBut3.addEventListener('click', () => {
   navUl.classList.remove('active');
+});
+
+
+
+popBut[0].addEventListener('click', () => {
+  popUl[0].classList.add('pop-active');
 });
