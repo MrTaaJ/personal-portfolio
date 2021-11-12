28aaf3ac7b16ee76f9e901b086bc80864d86d88e
Saheed Tajudeen <saheed.taaj@gmail.com>
2021-11-12 18:18:08 +0100

    feat: successfully added all the javascript template code

--- index.js
+++ index.js
@@ -23,8 +23,96 @@ cancelBut3.addEventListener('click', () => {
   navUl.classList.remove('active');
 });
 
+//Creating more pages
+const itemList = [
+  {desktopTitle: "Keeping track of hundreds of components website",
+mobileTitle: "Multi-Post Stories",
+detailPa: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy",
+liveRef: "https://google.com",
+sourceRef: "https://github.com/mrtaaj",
+skill: "<ul><li>html</li><li>bootstrap</li><li>Ruby on rails</li></ul>",
+backStyle: "url(/img/Pop-Desktop-Portfolio.png)"
+},
+{desktopTitle: "Title2: Keeping track of hundreds of components website",
+mobileTitle: "Multi-Post Stories 2",
+detailPa: "This is the second Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy",
+liveRef: "https://yahoo.com",
+sourceRef: "https://twitter.com",
+skill: "<ul><li>html</li><li>bootstrap</li><li>JavaScript</li></ul>",
+backStyle: "url(/img/proj-2.png)"
+},
+{desktopTitle: "Title3: Keeping track of hundreds of components website",
+mobileTitle: "Multi-Post Stories 3",
+detailPa: "This is the third Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy",
+liveRef: "https://facebook.com",
+sourceRef: "https://github.com/waslead-coder",
+skill: "<ul><li>html</li><li>bootstrap</li><li>Ruby on rails</li><li>JavaScript</li></ul>",
+backStyle: "url(/img/proj-2.png)"
+},
+{desktopTitle: "Title4: Keeping track of hundreds of components website",
+mobileTitle: "Multi-Post Stories 4",
+detailPa: "This is the fourth Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy",
+liveRef: "https://google.com",
+sourceRef: "https://github.com/mrtaaj",
+skill: "<ul><li>html</li><li>bootstrap</li><li>Ruby on rails</li></ul>",
+backStyle: "url(/img/Pop-Desktop-Portfolio.png)"
+},
+{desktopTitle: "Title5: Keeping track of hundreds of components website",
+mobileTitle: "Multi-Post Stories 5",
+detailPa: "This is the fifth Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy",
+liveRef: "https://yahoo.com",
+sourceRef: "https://twitter.com",
+skill: "<ul><li>html</li><li>bootstrap</li><li>JavaScript</li></ul>",
+backStyle: "url(/img/proj-2.png)"
+},
+{desktopTitle: "Title6: Keeping track of hundreds of components website",
+mobileTitle: "Multi-Post Stories 6",
+detailPa: "This is the sixth Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy",
+liveRef: "https://facebook.com",
+sourceRef: "https://github.com/waslead-coder",
+skill: "<ul><li>html</li><li>bootstrap</li><li>Ruby on rails</li><li>JavaScript</li></ul>",
+backStyle: "url(/img/proj-2.png)"
+},
+{desktopTitle: "Title7: Keeping track of hundreds of components website",
+mobileTitle: "Multi-Post Stories 7",
+detailPa: "This is the seventh Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy",
+liveRef: "https://facebook.com",
+sourceRef: "https://github.com/waslead-coder",
+skill: "<ul><li>html</li><li>bootstrap</li><li>Ruby on rails</li><li>JavaScript</li></ul>",
+backStyle: "url(/img/Pop-Desktop-Portfolio.png)"
+},
+];
+function setter (i){
+  document.querySelector("#mobileChangerCo").innerText = itemList[i].mobileTitle;
+  document.querySelector("#desktopChangerCo").innerText = itemList[i].desktopTitle;
+  document.querySelector("#changeCo").innerText = itemList[i].detailPa;
+  document.querySelector("#liveChange").href= itemList[i].liveRef;
+  document.querySelector("#sourceChange").href= itemList[i].sourceRef;
+  document.querySelector(".pop-proj-skill").innerHTML= itemList[i].skill;
+  document.querySelector(".pop-proj-1-2-back").style.backgroundImage = itemList[i].backStyle;
+}
+//Action
+for (let i = 0; i<itemList.length; i++){
+  popBut[i].addEventListener('click', (e)=>{
+    setter(i)
+    popUl[0].classList.add('pop-active');
+  })
+    
+}
 
+// popBut[1].addEventListener('click', () => {
+//   popUl[0].classList.add('pop-active');
+//   console.log(document.querySelector(".pop-proj-1-2-para").p);
+//   console.log(itemList[0].title);
+// });
 
-popBut[0].addEventListener('click', () => {
-  popUl[0].classList.add('pop-active');
+//Cancel/Remove Action
+document.querySelector("#pop-proj-1-back").addEventListener('click', () => {
+  popUl[0].classList.remove('pop-active');
+});
+document.querySelector("#cancel-pop-butt").addEventListener('click', () => {
+  popUl[0].classList.remove('pop-active');
+});
+document.querySelector(".pop-proj-1").addEventListener('click', (e) => {
+  e.stopPropagation();
 });
