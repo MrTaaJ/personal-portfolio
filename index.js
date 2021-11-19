0fe2b4d7a1c81700c131d142173acb1a497c1f64
Saheed Tajudeen <saheed.taaj@gmail.com>
2021-11-19 22:49:17 +0100

    feat: refactoring of the javascript file

--- index.js
+++ index.js
@@ -1,22 +1,14 @@
-const hamburger = document.getElementById('mobile-nav');
-const navUl = document.getElementById('mobile-nav-tog');
-const cancelBut = document.getElementById('cancel-butt');
-const cancelBut1 = document.getElementById('cancel1-butt');
-const cancelBut2 = document.getElementById('cancel2-butt');
-const cancelBut3 = document.getElementById('cancel3-butt');
+const hamburger = document.querySelector('#mobile-nav');
+const navUl = document.querySelector('#mobile-nav-tog');
+const navDeactivators = document.querySelectorAll('.nav-deactivator');
+
+const removeActiveClass = () => {
+  navUl.classList.remove('active');
+}
 
 hamburger.addEventListener('click', () => {
   navUl.classList.add('active');
 });
-cancelBut.addEventListener('click', () => {
-  navUl.classList.remove('active');
-});
-cancelBut1.addEventListener('click', () => {
-  navUl.classList.remove('active');
-});
-cancelBut2.addEventListener('click', () => {
-  navUl.classList.remove('active');
-});
-cancelBut3.addEventListener('click', () => {
-  navUl.classList.remove('active');
+navDeactivators.forEach(navDeactivator => {
+  navDeactivator.addEventListener('click', removeActiveClass);
 });
