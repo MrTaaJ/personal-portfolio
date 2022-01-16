71aa7c528bbd330e998b329c2a5004a0d768cd5f
Saheed Tajudeen <saheed.taaj@gmail.com>
2022-01-16 01:41:45 +0100

    feat: Update the .js app by cleaning the commented code

--- js/validator.js
+++ js/validator.js
@@ -30,6 +30,7 @@ export class formValidator {
         const emailTest = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         if (!emailTest.test(this.emailVal.value) || this.emailVal.value ===""){
             document.querySelector('.emailArea-note').classList.add('textarea-note-active');
+            this.formBut(true)
         } else {
             document.querySelector('.emailArea-note').classList.remove('textarea-note-active');
             if (this.eventBut(this.textVal)===true || this.eventBut(this.nameVal)===true ){
